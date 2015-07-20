832d0cf9f921e8dee548b9abdf111fd3ace4ad2f
tahoecoop <username@users.noreply.github.com>
2015-07-20 12:49:44 -0700

    Initial Commit

---
+++ Deproccrastinator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Deproccrastinator
+//
+//  Created by Benjamin COOPER on 7/20/15.
+//  Copyright (c) 2015 Ben Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Deproccrastinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Deproccrastinator
+//
+//  Created by Benjamin COOPER on 7/20/15.
+//  Copyright (c) 2015 Ben Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
